cef3a62f68e6da434e2644c80b88b9f72e4a7c51
Andre Jaoui <andre.jaoui@vgy.se>
2016-09-01 16:07:45 +0200

    Övn 1-4

---
+++ If-satser/If-satser/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_satser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // Exempel_1 från C#skolan
+            Console.WriteLine("Mata in ett heltal");
+            string inmatat = Console.ReadLine();
+            int x = int.Parse(inmatat);
+
+            if (x < 10)
+            {
+                Console.WriteLine("Det var mindre än 10!");
+            }
+            else
+            {
+                Console.WriteLine("Det var större eller lika med 10!");
+            }
+            Console.ReadLine();
+
+            //Exempel_2 Mer om grafiska fönster senare.
+
+            //Exempel_3
+            Console.WriteLine("Hur gammal är du? ");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder < 18)
+            {
+                //Detta block körs då vi t.ex. har en ålder på 17
+                Console.WriteLine("Du har inget körkort!");
+            }
+            else if (ålder == 18)
+            {
+                //Vid ett värde på exakt 18 så körs detta block.
+                Console.WriteLine("Dags att ta körkort?");
+            }
+            else
+            {
+                //Om inget av ovanstående villkor gäller så körs else-blocket.
+                Console.WriteLine("Kör försiktigt!");
+            }
+
+            //Övning_1
+            Console.WriteLine("Vilket mönsterdjup har dina däck i mm?");
+            double monsterDjup = double.Parse(Console.ReadLine());
+
+            if (monsterDjup <= 1.6)
+                Console.WriteLine("Dags att åka till däckverkstaden och byta däck");
+            else
+                Console.WriteLine("Du har " +(monsterDjup-1.6) + " mm slitbana kvar");
+
+            //Övning_2
+            Console.WriteLine("Ange din ålder");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age <= 12)
+                Console.WriteLine("Vit bricka");    //Endast en sats. Behövs inga { }
+            else if (age > 12 && age < 19)
+                Console.WriteLine("Grön bricka");
+            else if (age > 18 && age < 26)
+                Console.WriteLine("Röd bricka");
+            else if (age > 25 && age < 100)
+                Console.WriteLine("Blå bricka");
+            else
+                Console.WriteLine("Du är för gammal eller för ung för den här mässan");
+           
+            //Övning_3
+            Console.WriteLine("Mata in det första av två tal");
+            decimal tal1 = (decimal)(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Mata in det andra talet");
+            decimal tal2 = (decimal)(double.Parse(Console.ReadLine()));
+
+            if (tal1 < tal2)
+                Console.WriteLine("Tal 2 var störst");
+            else if (tal1 > tal2)
+                Console.WriteLine("Tal 1 var störst");
+            else
+                Console.WriteLine("Talen var lika stora");
+            
+            //Övning_4 
+            Console.WriteLine("Ange ett heltalsvärde. Jag undersöker om 7 är en delare till talet");
+            int heltal = int.Parse(Console.ReadLine());
+
+            // modulus-operatorn % ger resten vid heltalsdivison tex: 15 % 7 = 1 då 15 = 2*7+1 
+            if (heltal%7 != 0)
+                Console.WriteLine("7 är inte en delare till: " + heltal);
+            else
+                Console.WriteLine("7 är en delare till " + heltal);
+            */
+
+        }
+    }
+}
