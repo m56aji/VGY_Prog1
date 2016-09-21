5caec841babc523068f7b64de5c5280345cd16f4
André Jaoui <andre_jaoui@hotmail.com>
2016-09-21 23:17:33 +0200

    Övn1

---
+++ While-loopar/While-loopar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_loopar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Övning1
+            int nbr = 1;
+            while (nbr <= 100)
+            {
+                Console.WriteLine(nbr);
+                nbr++;
+            }
+        }
+    }
+}
