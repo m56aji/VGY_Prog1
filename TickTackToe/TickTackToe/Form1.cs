8030f0b79cf08a1d64b4ed0d33b3453be9d0d375
Andre Jaoui <andre.jaoui@vgy.se>
2017-01-12 10:11:03 +0100

    Med vinstalogritmerna som metoder

--- TickTackToe/TickTackToe/Form1.cs
+++ TickTackToe/TickTackToe/Form1.cs
@@ -6,24 +6,28 @@ namespace TickTackToe
     public partial class Form1 : Form       //Klassen subklassen Form1 ärver ":" egenskaper från föräldrarklassen Form
     {
         bool XTurn = false;
-        Button[,] array;                    //Deklarera en 2:dimensionell array som fält/klassvariabel
+        Button[,] buttonArray;                //Deklarera en 2:dimensionell array som fält/klassvariabel
+        int arrSize;                    //Gör arrSize till en klassvariabel
+        string playerXO;                  //Är det spelare X eller spelare O som gjort sista draget
 
         public Form1()
         {
             InitializeComponent();
             //skapar en tvådimensionell array[3,3] med tre rader och tre kolumner
-            array = new Button[,]  {        { button1, button2, button3},           //Element i rad 0
+            buttonArray = new Button[,]  {  { button1, button2, button3},           //Element i rad 0
                                             { button4, button5, button6},           //Element i rad 1
                                             { button7, button8, button9} };         //Element i rad 2
+            arrSize = buttonArray.GetLength(0);                                           //Returnerar antal element på rad 0
         }
 
         //Raderar texten på knapparna efter en vinstomgång
-        private void ResetBoard() {
-            int size = array.GetLength(0);                      //Ger antalet element i rad 0 och öppnar för att göra större spelplan
-
-            for (int x = 0; x < size; x++) {
-                for (int y = 0; y < size; y++) {
-                    array[y, x].Text = string.Empty;
+        private void ResetBoard()
+        {
+            for (int x = 0; x < arrSize; x++)
+            {
+                for (int y = 0; y < arrSize; y++)
+                {
+                    buttonArray[y, x].Text = string.Empty;
                 }
             }//inre for
         }//yttre for
@@ -36,83 +40,96 @@ namespace TickTackToe
         {
             Button b = sender as Button;            //Nyckelordet "as" visar vilken typ objekt-variabeln "sender" ska ha
             if (b == null || !string.IsNullOrEmpty(b.Text))         //Om inte knappen är textlös 
-                return;                                             //lämna Button_Click-metoden (early exit)
+                return;                                             //lämna Button_Click-metoden (early exit) trots att returtypen är void
 
-            string player = (XTurn) ? "X" : "O";                    //Om XTurn är true tillela player X annars tilldela player O
-            b.Text = player;
+            playerXO = (XTurn) ? "X" : "O";                         //Om XTurn är true tillela player X annars tilldela player O
+            b.Text = playerXO;
 
-            XTurn = !XTurn;
+            XTurn = !XTurn;                                         //Växlar mellan X och O och viseversa
 
-            int size = array.GetLength(0);
+            LodrättVinst();
+            VågrättVinst();
+            Diagonalt1Vinst();
+            Diagonalt2Vinst();
+            IngenVinnare();
 
-            //Studera och förstå dig på lösningen. Viktigt!
-            //Kollar om någon vunnit lodrätt/kolumner(k)
-            for (int x = 0; x < size; x++)
+        }//Button_Click
+
+        //Kollar om någon vunnit lodrätt/kolumner
+        private void LodrättVinst()
+        {
+            for (int x = 0; x < arrSize; x++)
             {
                 //OBS ett villkor i villkoret. Gör så att for-loopen bryts om inte texten är identisk på alla knappar i loopen
-                for (int y = 0; y < size && player == array[y, x].Text ; y++)
+                for (int y = 0; y < arrSize && playerXO == buttonArray[y, x].Text; y++)
                 {
-                    if (y == size - 1)                                                  //Om k = 2 så har for-loopen fullföljts
+                    if (y == arrSize - 1)                                                  //Om k = 2 så har for-loopen fullföljts
                     {
-                        MessageBox.Show(this, "Player " + player + " vertikal won!");
+                        MessageBox.Show(this, "Player " + playerXO + " vertikal won!");
                         ResetBoard();
                     }
                 }
             }
+        }//Lodrätt
 
-            //rader
-            for (int y = 0; y < size; y++)
+        //Kollar om någon vunnit vågrätt
+        private void VågrättVinst()
+        {
+            for (int y = 0; y < arrSize; y++)
             {
-                for (int x = 0; x < size && array[y, x].Text == player; x++)
+                for (int x = 0; x < arrSize && buttonArray[y, x].Text == playerXO; x++)
                 {
-                    if (x == size - 1)
+                    if (x == arrSize - 1)
                     {
-                        MessageBox.Show(this, "Player " + player + " horizontal won!");
+                        MessageBox.Show(this, "Player " + playerXO + " horizontal won!");
                         ResetBoard();
                     }
                 }
             }
+        }//Vågrätt
 
-            //Kollar om någon vunnit diagonalt snett åt höger, dvs: [0,0] [1,1] och [2,2]
-            for (int i = 0; i < size && array[i, i].Text == player; i++)
+        //Kollar om någon vunnit diagonalt snett åt höger, dvs: [0,0] [1,1] och [2,2]
+        private void Diagonalt1Vinst()
+        {
+            for (int i = 0; i < arrSize && buttonArray[i, i].Text == playerXO; i++)
             {
-                if (i == size - 1)
+                if (i == arrSize - 1)
                 {
-                    MessageBox.Show(this, "Player " + player + " diagonal '\' won!");
+                    MessageBox.Show(this, "Player " + playerXO + " diagonal '\' won!");
                     ResetBoard();
                 }
             }
-
-            //Kollar om någon vunnit diagonalt snett åt vänster, dvs: [0,2] [1,1] och [2,0]
-            for (int i = 0; i < size && array[size - 1 - i, i].Text == player; i++)
-            {
-                if (i == size - 1)
-                {
-                    MessageBox.Show(this, "Player " + player + " diagonal / won!");
+        }
+        
+        //Kollar om någon vunnit diagonalt snett åt vänster, dvs: [0,2] [1,1] och [2,0]
+        private void Diagonalt2Vinst() {
+            for (int i = 0; i < arrSize && buttonArray[arrSize - 1 - i, i].Text == playerXO; i++) {
+                if (i == arrSize - 1) {
+                    MessageBox.Show(this, "Player " + playerXO + " diagonal / won!");
                     ResetBoard();
                 }
             }
+        }
 
-            //Ingen vinnare om det inte finns några tomma knappar
+        //Samtliga buttons iklickade men ingen vann
+        private void IngenVinnare() {
             bool finished = true;
 
-            //Finns det några 
-            for (int x = 0; x < size; x++)
-            {
-                for (int y = 0; y < size; y++)
-                {
-                    if (string.IsNullOrEmpty(array[x, y].Text))
-                        finished = false;
+            //Finns det några tomma rutor/knappar?
+            for (int x = 0; x < arrSize; x++) {
+                for (int y = 0; y < arrSize; y++) {
+                    if (string.IsNullOrEmpty(buttonArray[x, y].Text))
+                        finished = false;           //Tom ruta hittad alltså är spelet inte slutspelat
                 }
             }
 
-            if (finished)
-            {
-                MessageBox.Show(this, "It's a draw!");
+            if (finished) {
+                MessageBox.Show(this, "It's a draw!");  //Det är oavgjort
                 ResetBoard();
             }
         }
-    }//Main
-}//class
+
+    }//class
+}//namespace
 
 
